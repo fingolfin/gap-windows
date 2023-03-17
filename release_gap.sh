3f089d2eb0ef1292eba14e0341568e7fbb709ac1
Max Horn <max@quendi.de>
2023-03-17 15:50:33 +0100

    Oops, remove SAGE_MAKE_CONFIGURE_CMD / SAGE_RUN_CONFIGURE_CMD overides
    GAP CI is no longer setting this variables, I misread how this works before

--- release_gap.sh
+++ release_gap.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 make --trace \
-    SAGE_MAKE_CONFIGURE_CMD?='"cd $(SAGE_ROOT) && ./autogen.sh && ./configure"' \
-    SAGE_RUN_CONFIGURE_CMD?='"cd $(SAGE_ROOT) && make && make bootstrap-pkg-full"' \
     SAGE_STARTED='"$(SAGE_ROOT)/pkg/log"' \
     SAGE_STARTED='"$(SAGE_ROOT_BUILD)/gap"'
